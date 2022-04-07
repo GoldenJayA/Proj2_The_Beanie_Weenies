e93acb34f592507e89e8dccf386654c171edd362
Angel Jaimes (AJ) <angeldjaimes19@gmail.com>
2022-04-07 10:12:55 -0500

    Create Proj2_Beanie_weenies_v1.ino

---
+++ Proj2_Beanie_weenies_v1.ino
@@ -0,0 +1,14 @@
+/* Group Name: The Beanie Weenies
+Members: Belice Rivarola, Jaime Bunker, Angel Jaimes
+Fable: Jack & The Giant Beanstalk
+*/
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
